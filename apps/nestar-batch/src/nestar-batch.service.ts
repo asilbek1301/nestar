94a246e2dd824388e067bba43bba616a444c276d
alan <asilbekotanazarov05@dhu.ac.kr>
2026-08-11 23:28:05 +0900

    fix: installation packages & modify format standarts

--- apps/nestar-batch/src/nestar-batch.service.ts
+++ apps/nestar-batch/src/nestar-batch.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class NestarBatchService {
-  getHello(): string {
-    return 'Welcome to Nestar BATCH API server!';
-  }
+	getHello(): string {
+		return 'Welcome to Nestar BATCH API server!';
+	}
 }
