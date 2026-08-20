4e92a40d9e55f472346d2dd478fbdc128910634d
alan <asilbekotanazarov05@dhu.ac.kr>
2026-08-20 15:00:16 +0900

    fix: creation and verification jwt

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -19,7 +19,8 @@ export class MemberService {
 
 		try {
 			const result = await this.memberModel.create(input);
-			// TODO: Authentication via TOKEN
+			result.accessToken = await this.authService.createToken(result);
+
 			return result;
 		} catch (err) {
 			console.log('Error, Service.model:', err.message);
@@ -39,6 +40,8 @@ export class MemberService {
 
 		const isMatch = await this.authService.comparePasswords(input.memberPassword, response.memberPassword as string);
 		if (!isMatch) throw new InternalServerErrorException(Message.WRONG_PASSWORD);
+
+		response.accessToken = await this.authService.createToken(response);
 
 		return response;
 	}
