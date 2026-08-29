ff1299371cd4f34ccd53fef674c323f803570800
alan <asilbekotanazarov05@dhu.ac.kr>
2026-08-30 00:36:39 +0900

    feat: develop removePropertyByAdmin GraphQL api

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -105,4 +105,14 @@ export class PropertyResolver {
 
 		return await this.propertyService.updatePropertyByAdmin(input);
 	}
+
+	@Roles(MemberType.ADMIN)
+	@UseGuards(RolesGuard)
+	@Mutation((returns) => Property)
+	public async removePropertyByAdmin(@Args('propertyId') input: string): Promise<Property> {
+		console.log('Query: removePropertyByAdmin');
+		const propertyId = shapeIntoMongoObjectId(input);
+
+		return await this.propertyService.removePropertyByAdmin(propertyId);
+	}
 }
